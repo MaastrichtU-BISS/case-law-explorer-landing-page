10ed1eaea0ec3ed6b0a199e360eccbd55102330d
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-10-10 16:01:35 +0200

    Support text arrays

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,11 +1,20 @@
 <template>
-  <h2>{{ props.title }}</h2>
-  <p class="mt-4 text-lg">{{ props.text }}</p>
+  <h2>{{ title }}</h2>
+  <template v-if="Array.isArray(text)">
+    <template v-for="item in text">
+      <p class="mt-4 text-lg" v-html="item"></p>
+    </template>
+  </template>
+  <p v-else class="mt-4 text-lg" v-html="text"></p>
 </template>
 
 <script setup lang="ts">
+import { toRefs } from 'vue';
+
 const props = defineProps<{
   title: string;
-  text: string;
+  text: string | string[];
 }>();
+
+const { title, text } = toRefs(props);
 </script>
