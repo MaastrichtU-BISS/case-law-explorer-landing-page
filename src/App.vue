c5db8e31b723b120074b5cd73c2b194970bde380
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-10-10 16:01:54 +0200

    Text to English

--- src/App.vue
+++ src/App.vue
@@ -5,40 +5,35 @@
     alt="Magnifying Glass"
   />
   <div class="p-6">
-    <img
-      src="./assets/images/Case Law Explorer logo.svg"
-      alt="Case Law Explorer logo"
-    />
+    <img src="./assets/images/Case Law Explorer logo.svg" alt="Case Law Explorer logo" />
     <h1 class="text-3xl text-lightblue">
-      Een open-source tool voor juridische netwerkanalyse
+      A software application for conducting network analysis on court cases
     </h1>
     <div class="max-w-5xl mt-16 text-darkblue">
+      <Section title="About" :text="about" />
       <Section
-        title="Over Case Law Explorer"
-        text="Case Law Explorer is ontwikkeld door Prof. Dr. Gijs van Dijck, Maastricht Law and Tech Lab en het Brightlands Institute for Smart Society
-      Case Law Explorer maakt de toepassing van netwerkanalyse op een juridische context mogelijk. De analyse kan betrekking hebben op wetgeving,
-      jurisprudentie, netwerken van criminelen, patenten, netwerken van advocaten enz."
-      />
-      <Section
-        title="Wij zoeken ambassadeurs"
-        text="Om de Case Law Explorer een stap verder te brengen en het gebruik ervan binnen universiteiten en hogescholen te stimuleren zijn we opzoek naar ambassadeurs. Als ambassadeur heb je enerzijds de
-      mogelijkheid om de tool nu al binnen je instelling te gebruiken en anderzijds kun je bijdragen aan de doorontwikkeling en verdere uitwerking van de tool zodat deze aansluit op specifieke behoeften."
+        title="Community"
+        text="The project is currently seeking ambassadors from higher education institutions who support the use and continuous development of Case Law Explorer. Participating institutions will be listed on this page."
       />
     </div>
     <div class="flex flex-row flex-wrap">
       <div>
-        <h2>Huidige ambassadeurs</h2>
+        <h2>Current ambassadors</h2>
         <div class="flex flex-wrap mt-4">
-          <img
-            src="./assets/images/UM.svg"
-            class="duration-300 grayscale hover:grayscale-0"
-            alt="UM logo"
-          />
-          <img
-            src="./assets/images/Zuyd.svg"
-            class="ml-4 duration-300 grayscale hover:grayscale-0"
-            alt="Zuyd logo"
-          />
+          <a href="https://www.maastrichtuniversity.nl/">
+            <img
+              src="./assets/images/UM.svg"
+              class="duration-300 grayscale hover:grayscale-0"
+              alt="UM logo"
+            />
+          </a>
+          <a href="https://www.zuyd.nl/" class="ml-4">
+            <img
+              src="./assets/images/Zuyd.svg"
+              class="ml-4 duration-300 grayscale hover:grayscale-0"
+              alt="Zuyd logo"
+            />
+          </a>
         </div>
       </div>
       <div class="lg:ml-96">
@@ -47,22 +42,13 @@
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import Graph from "./components/Graph.vue";
 import Section from "./components/Section.vue";
-</script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+const about = [
+  "Case Law Explorer allows finding precedents for small and large samples, and identifying topics (clusters or communities) within a network of court decisions.",
+  "Case Law Explorer was initially developed by the <a href='https://www.maastrichtuniversity.nl/about-um/faculties/law/research/law-and-tech-lab'>Maastricht Law and Tech Lab</a> and the <a href='https://www.biss-institute.com/'>Brightlands Institute for Smart Society (BISS)</a> of Maastricht University. <a href='https://www.zuyd.nl/'>Zuyd University for Applied Sciences</a> and <a href='https://www.maastrichtuniversity.nl/'>Maastricht University</a> teamed up to further develop the software and to implement it in their educational programs.",
+];
+</script>
