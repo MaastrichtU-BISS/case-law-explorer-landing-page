9fccbfa9a1d2939a4d699d8062058dcc6a42ba2e
CvdL-UM <93982741+CvdL-UM@users.noreply.github.com>
2025-10-08 17:09:42 +0200

    style: Use similar styling as platform

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,61 @@
 <template>
-  <div class="absolute max-w-xl m-auto -top-4 -right-40 hidden maxsize:grid">
-    <img
-      src="./assets/images/magnifying-glass.svg"
-      class="select-none row-start-1 col-start-1"
-      alt="Magnifying Glass"
-    />
+  <!-- <div class="absolute max-w-xl m-auto -top-4 -right-40 hidden maxsize:grid">
+    <img src="./assets/images/magnifying-glass.svg" class="select-none row-start-1 col-start-1"
+      alt="Magnifying Glass" />
     <div class="row-start-1 col-start-1 mt-6 ml-6">
       <Graph />
     </div>
-  </div>
+  </div> -->
 
   <div class="p-6">
-    <img src="./assets/images/Case-Law-Explorer-logo.svg" alt="Case Law Explorer logo" />
-    <h1 class="text-3xl text-lightblue">
+    <h1 class="text-6xl font-bold">Case Law Explorer</h1>
+    <h2 class="text-3xl mt-2">
       A software application for conducting network analysis on court cases
-    </h1>
-    <div class="max-w-5xl mt-14 text-darkblue">
+    </h2>
+
+    <img src="./assets/images/network.png" class="w-full xl:w-3/4 mt-10"
+      alt="The Case Law Explorer tool showing a network graph of related legal cases" />
+
+    <div class="max-w-5xl mt-4">
       <Section title="About" :text="about" />
       <div class="flex flex-col md:flex-row gap-5 mt-3">
         <a href="https://app.caselawexplorer.tech">
           <button
-            class="bg-lightblue border-lightblue border-2 hover:bg-darkblue hover:border-darkblue text-white font-bold rounded p-4 uppercase flex items-center justify-center gap-2 select-none"
-          >
-            Try it out <span class="text-2xl h-[36px]">→</span>
+            class="bg-black text-white text-xl font-bold rounded px-4 py-2 flex items-center justify-center gap-2 select-none">
+            Try it out
           </button>
         </a>
       </div>
 
-      <Section
-        title="Community"
-        text="The project is currently seeking ambassadors from higher education institutions who support the use and continuous development of Case Law Explorer. Participating institutions will be listed on this page."
-      />
+      <div class="mt-10">
+        <Section title="Community"
+          text="The project is currently seeking ambassadors from higher education institutions who support the use and continuous development of Case Law Explorer. Participating institutions will be listed on this page." />
+      </div>
     </div>
 
-    <div class="flex flex-row flex-wrap">
-      <div class="w-full md:w-1/2">
-        <h2>Current ambassadors</h2>
+    <div class="flex flex-row flex-wrap mt-10">
+      <div class="w-full">
+        <h3>Current ambassadors</h3>
         <div class="flex flex-wrap mt-4">
           <a href="https://www.maastrichtuniversity.nl/">
-            <img
-              src="./assets/images/UM.svg"
-              class="duration-300 grayscale hover:grayscale-0"
-              alt="UM logo"
-            />
+            <img src="./assets/images/UM.svg" class="duration-300 grayscale hover:grayscale-0" alt="UM logo" />
           </a>
           <a href="https://www.zuyd.nl/" class="ml-4">
-            <img
-              src="./assets/images/Zuyd.svg"
-              class="ml-4 duration-300 grayscale hover:grayscale-0"
-              alt="Zuyd logo"
-            />
+            <img src="./assets/images/Zuyd.svg" class="ml-4 duration-300 grayscale hover:grayscale-0" alt="Zuyd logo" />
           </a>
         </div>
       </div>
-      <div class="w-full md:w-1/2">
-        <h2>The tool</h2>
-        <img
-          src="./assets/images/network.jpg"
-          class="w-full xl:w-3/4 mt-4"
-          alt="The Case Law Explorer tool showing a network graph of related legal cases"
-        />
-      </div>
+      <!-- <div class="w-full md:w-1/2">
+        <h3>The tool</h3>
+        <img src="./assets/images/network.jpg" class="w-full xl:w-3/4 mt-4"
+          alt="The Case Law Explorer tool showing a network graph of related legal cases" />
+      </div> -->
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Graph from "./components/Graph.vue";
+// import Graph from "./components/Graph.vue";
 import Section from "./components/Section.vue";
 
 const about = [
--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,11 +1,11 @@
 <template>
-  <h2>{{ title }}</h2>
+  <h3>{{ title }}</h3>
   <template v-if="Array.isArray(text)">
     <template v-for="item in text">
-      <p class="mt-4 text-lg" v-html="item"></p>
+      <p class="mt-2 text-lg" v-html="item"></p>
     </template>
   </template>
-  <p v-else class="mt-4 text-lg" v-html="text"></p>
+  <p v-else class="mt-2 text-lg" v-html="text"></p>
 </template>
 
 <script setup lang="ts">
